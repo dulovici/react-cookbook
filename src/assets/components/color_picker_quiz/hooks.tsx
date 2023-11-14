a07baeca2feead77ce277205537363d29946704f
Igor Dulovic <igorx24@gmail.com>
2023-11-14 21:33:56 +0100

    timer reset

--- src/assets/components/color_picker_quiz/hooks.tsx
+++ src/assets/components/color_picker_quiz/hooks.tsx
@@ -1,15 +1,25 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export const usePopup = () => {
   const [active, setActive] = useState(false);
+  const [timerId, setTimerId] = useState(0);
 
-  const popup = () => {
+  const popup = useCallback(() => {
+    // Clear existing timeout if any
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+
     setActive(true);
 
-    setTimeout(() => {
+    // Set a new timeout
+    const id = setTimeout(() => {
       setActive(false);
     }, 2000);
-  };
+
+    // Store the new timer ID
+    setTimerId(id);
+  }, [timerId]);
 
   return { isPopup: active, popup };
 };
