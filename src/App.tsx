7f8aad4ae39c80c33af160a6d6fc18c16eb9fe29
Igor Dulovic <igorx24@gmail.com>
2023-11-22 15:13:31 +0100

    renaming component folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
 import "./App.css";
-import DropdownPicker from "./assets/components/dropdown_picker/DropdownPicker";
-// import StopLight from "./assets/components/stop_light/StopLight";
-// import PassCode from "./assets/components/pass_code/PassCode";
-// import ImageSlider from "./assets/components/image_slider/ImageSlider";
-// import FileTree from "./assets/components/file_tree/FileTree";
-// import MemoryGame from "./assets/components/memory_game/first/MemoryGame";
-// import Memory from "./assets/components/memory_game/Memory";
-// import PinBoard from "./assets/components/pin_board/PinBoard";
-// import ColorPickerQuiz from "./assets/components/color_picker_quiz/ColorPickerQuiz";
-// import SynonymFinder from "./assets/components/synonym_finder/SynonymFinder";
+import DropdownPicker from "./recepies/dropdown_picker/DropdownPicker";
 
 function App() {
   return (
